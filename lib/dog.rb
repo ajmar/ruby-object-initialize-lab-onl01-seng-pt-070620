fd19049d28095e5ef6f867361479f8994b59523d
Alexander Marusin <amarusin18@gmail.com>
2020-07-26 00:24:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,10 @@
 class Dog
   def initialize(name, breed)
     @name = name
-    @breed = breed or "Mutt"
+    if breed = nil
+      @breed = "Mutt"
+    else
+    @breed = breed
   end
 
   def name=(name)
